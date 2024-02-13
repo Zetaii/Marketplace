f1269b0c3208e7f909fd4db9566c36c583aa68c4
Zetaii <zerga556@yahoo.com>
2024-02-13 13:29:59 -0600

    update pack.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["localhost", "marketplace-delta-lemon.vercel.app"],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
